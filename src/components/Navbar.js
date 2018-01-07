621ddf972b76a64b5d288a00c5cf1e52f769f9f0
Misael Rivera <mrivera0709@gmail.com>
2018-01-07 13:39:01 -0500

    navba adjustment

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = props => (
   <header className="App-header">
     <h1 className="App-title"> NBA Memory Clicky Game</h1>
     {/*<p className="score">Score goes here!</p>*/}
-    <p className="score">Score: { props.score } out of 12 | Top Score: { props.topScore }</p>
+    <p className="score">Score: { props.score } out of 12 --- Top Score: { props.topScore }</p>
     <p className="message">{ props.message }</p>
   </header>
 );
